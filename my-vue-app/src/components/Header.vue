a7c18c93a694a8212887e09edac3fdfbc73b36af
user <krasilov.kolya@yandex.ru>
2025-07-08 12:46:02 +0300

    Refacoring Header
    sfdsfd
    sdf

--- my-vue-app/src/components/Header.vue
+++ my-vue-app/src/components/Header.vue
@@ -5,15 +5,20 @@ import logo from '../assets/images/logo.png'
 import userIcon from '../assets/icons/people.svg'
 import dropdownArrow from '../assets/icons/check_mark.svg'
 
+// Состояние компонента
 const isMobileMenuOpen = ref(false)
 const isMobile = ref(false)
 const activeDropdown = ref(null)
 
-const selectedPlugin = ref('Plugins')
-const selectedTheme = ref('Themes')
-const selectedPricing = ref('Pricing')
-const selectedLanguage = ref('En')
+// Выбранные значения
+const selectedValues = {
+  plugin: ref('Plugins'),
+  theme: ref('Themes'),
+  pricing: ref('Pricing'),
+  language: ref('En')
+}
 
+// Методы
 const checkScreenSize = () => {
   isMobile.value = window.innerWidth < 768
 }
@@ -27,156 +32,89 @@ const closeDropdowns = () => {
 }
 
 const selectItem = (type, value) => {
-  switch(type) {
-    case 'plugin':
-      selectedPlugin.value = value
-      break
-    case 'theme':
-      selectedTheme.value = value
-      break
-    case 'pricing':
-      selectedPricing.value = value
-      break
-    case 'language':
-      selectedLanguage.value = value
-      break
+  if (selectedValues[type]) {
+    selectedValues[type].value = value
   }
   setTimeout(closeDropdowns, 100)
 }
 
+// Хуки жизненного цикла
 onMounted(() => {
   checkScreenSize()
   window.addEventListener('resize', checkScreenSize)
-  document.addEventListener('click', (e) => {
-    if (!e.target.closest('.header__dropdown')) {
-      closeDropdowns()
-    }
-  })
+  document.addEventListener('click', handleDocumentClick)
 })
 
 onBeforeUnmount(() => {
   window.removeEventListener('resize', checkScreenSize)
-  document.removeEventListener('click', (e) => {
-    if (!e.target.closest('.header__dropdown')) {
-      closeDropdowns()
-    }
-  })
+  document.removeEventListener('click', handleDocumentClick)
 })
+
+// Вспомогательные функции
+const handleDocumentClick = (e) => {
+  if (!e.target.closest('.header__dropdown')) {
+    closeDropdowns()
+  }
+}
 </script>
 
 <template>
   <header class="header">
     <div class="header__container">
+      <!-- Логотип -->
       <div class="header__logo">
         <img :src="logo" alt="Company Logo" class="header__logo-img">
       </div>
 
+      <!-- Кнопка мобильного меню -->
       <button 
+        v-if="isMobile"
         class="header__mobile-menu-button" 
         @click="isMobileMenuOpen = !isMobileMenuOpen"
-        v-if="isMobile"
         aria-label="Toggle menu"
       >
         <span class="header__mobile-menu-icon"></span>
       </button>
 
+      <!-- Основная навигация -->
       <nav class="header__nav" :class="{ 'header__nav--active': isMobileMenuOpen }">
-        <div class="header__dropdown">
+        <div 
+          v-for="(item, index) in [
+            { type: 'plugin', name: 'Plugins', items: ['Plugin 1', 'Plugin 2'] },
+            { type: 'theme', name: 'Themes', items: ['Theme 1', 'Theme 2'] },
+            { type: 'pricing', name: 'Pricing', items: ['Basic', 'Pro'] }
+          ]" 
+          :key="index"
+          class="header__dropdown"
+        >
           <button 
             class="header__dropdown-button"
-            @click="toggleDropdown('plugins')"
+            @click="toggleDropdown(item.type)"
             aria-haspopup="true"
-            :aria-expanded="activeDropdown === 'plugins'"
+            :aria-expanded="activeDropdown === item.type"
           >
-            {{ selectedPlugin }}
+            {{ selectedValues[item.type].value }}
             <span class="header__dropdown-arrow">
-              <img 
-                :src="dropdownArrow" 
-                alt="Dropdown arrow"
-                class="dropdown-arrow-icon"
-              />
+              <img :src="dropdownArrow" alt="Dropdown arrow" class="dropdown-arrow-icon">
             </span>
           </button>
-          <ul 
-            class="header__dropdown-list"
-            v-show="activeDropdown === 'plugins'"
-          >
+          
+          <ul class="header__dropdown-list" v-show="activeDropdown === item.type">
             <li 
+              v-for="(subItem, subIndex) in item.items"
+              :key="subIndex"
               class="header__dropdown-item" 
-              @click.stop="selectItem('plugin', 'Plugin 1')"
-            >Plugin 1</li>
-            <li 
-              class="header__dropdown-item" 
-              @click.stop="selectItem('plugin', 'Plugin 2')"
-            >Plugin 2</li>
-          </ul>
-        </div>
-        
-        <div class="header__dropdown">
-          <button 
-            class="header__dropdown-button"
-            @click="toggleDropdown('themes')"
-            aria-haspopup="true"
-            :aria-expanded="activeDropdown === 'themes'"
-          >
-            {{ selectedTheme }}
-            <span class="header__dropdown-arrow">
-              <img 
-                :src="dropdownArrow" 
-                alt="Dropdown arrow"
-                class="dropdown-arrow-icon"
-              />
-            </span>
-          </button>
-          <ul 
-            class="header__dropdown-list"
-            v-show="activeDropdown === 'themes'"
-          >
-            <li 
-              class="header__dropdown-item" 
-              @click.stop="selectItem('theme', 'Theme 1')"
-            >Theme 1</li>
-            <li 
-              class="header__dropdown-item" 
-              @click.stop="selectItem('theme', 'Theme 2')"
-            >Theme 2</li>
-          </ul>
-        </div>
-        
-        <div class="header__dropdown">
-          <button 
-            class="header__dropdown-button"
-            @click="toggleDropdown('pricing')"
-            aria-haspopup="true"
-            :aria-expanded="activeDropdown === 'pricing'"
-          >
-            {{ selectedPricing }}
-            <span class="header__dropdown-arrow">
-              <img 
-                :src="dropdownArrow" 
-                alt="Dropdown arrow"
-                class="dropdown-arrow-icon"
-              />
-            </span>
-          </button>
-          <ul 
-            class="header__dropdown-list"
-            v-show="activeDropdown === 'pricing'"
-          >
-            <li 
-              class="header__dropdown-item" 
-              @click.stop="selectItem('pricing', 'Basic')"
-            >Basic</li>
-            <li 
-              class="header__dropdown-item" 
-              @click.stop="selectItem('pricing', 'Pro')"
-            >Pro</li>
+              @click.stop="selectItem(item.type, subItem)"
+            >
+              {{ subItem }}
+            </li>
           </ul>
         </div>
         
         <a href="#" class="header__nav-link">Resources</a>
       </nav>
 
+      <!-- Управляющие элементы -->
       <div class="header__controls" :class="{ 'header__controls--active': isMobileMenuOpen }">
         <a href="#" class="header__nav-link">Support</a>
         
@@ -187,27 +125,21 @@ onBeforeUnmount(() => {
             aria-haspopup="true"
             :aria-expanded="activeDropdown === 'language'"
           >
-            {{ selectedLanguage }}
+            {{ selectedValues.language.value }}
             <span class="header__dropdown-arrow">
-              <img 
-                :src="dropdownArrow" 
-                alt="Dropdown arrow"
-                class="dropdown-arrow-icon"
-              />
+              <img :src="dropdownArrow" alt="Dropdown arrow" class="dropdown-arrow-icon">
             </span>
           </button>
-          <ul 
-            class="header__dropdown-list"
-            v-show="activeDropdown === 'language'"
-          >
+          
+          <ul class="header__dropdown-list" v-show="activeDropdown === 'language'">
             <li 
+              v-for="lang in ['En', 'Ru']"
+              :key="lang"
               class="header__dropdown-item" 
-              @click.stop="selectItem('language', 'En')"
-            >En</li>
-            <li 
-              class="header__dropdown-item" 
-              @click.stop="selectItem('language', 'Ru')"
-            >Ru</li>
+              @click.stop="selectItem('language', lang)"
+            >
+              {{ lang }}
+            </li>
           </ul>
         </div>
         
@@ -215,6 +147,7 @@ onBeforeUnmount(() => {
           <span class="header__login-text">Login</span>
           <img :src="userIcon" alt="User icon" class="header__login-icon">
         </a>
+        
         <DemoButton class="header__demo-btn" />
       </div>
     </div>
