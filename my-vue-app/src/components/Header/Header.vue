f8d182ef9e540339b183d8bb136fedbcb0f416d8
user <krasilov.kolya@yandex.ru>
2025-07-08 16:48:26 +0300

    Divided navigation into a component

--- my-vue-app/src/components/Header/Header.vue
+++ my-vue-app/src/components/Header/Header.vue
@@ -1,14 +1,15 @@
 <script setup>
 import { ref, onMounted, onBeforeUnmount, computed } from 'vue';
 import DemoButton from '../DemoButton.vue';
-import logo from '../../assets/images/logo.png'
+import HeaderNav from './HeaderNav.vue';
+import logo from '../../assets/images/logo.png';
 import userIcon from '../../assets/icons/people.svg';
 import dropdownArrow from '../../assets/icons/check_mark.svg';
 
 // Reactive state
-const isMobileMenuOpen = ref(false)
-const activeDropdown = ref(null)
-const isMobile = ref(false)
+const isMobileMenuOpen = ref(false);
+const activeDropdown = ref(null);
+const isMobile = ref(false);
 
 // Selected values with computed for better reactivity
 const selected = {
@@ -16,7 +17,7 @@ const selected = {
   theme: ref('Themes'),
   pricing: ref('Pricing'),
   language: ref('En')
-}
+};
 
 // Dropdown configuration
 const dropdowns = {
@@ -36,45 +37,45 @@ const dropdowns = {
     items: ['En', 'Ru'],
     selected: selected.language
   }
-}
+};
 
 // Methods
 const checkScreenSize = () => {
-  isMobile.value = window.innerWidth < 768
-}
+  isMobile.value = window.innerWidth < 768;
+};
 
 const toggleDropdown = (dropdownName) => {
-  activeDropdown.value = activeDropdown.value === dropdownName ? null : dropdownName
-}
+  activeDropdown.value = activeDropdown.value === dropdownName ? null : dropdownName;
+};
 
 const closeDropdowns = () => {
-  activeDropdown.value = null
-}
+  activeDropdown.value = null;
+};
 
 const selectItem = (type, value) => {
   if (dropdowns[type]) {
-    dropdowns[type].selected.value = value
-    setTimeout(closeDropdowns, 100)
+    dropdowns[type].selected.value = value;
+    setTimeout(closeDropdowns, 100);
   }
-}
+};
 
 const handleDocumentClick = (e) => {
   if (!e.target.closest('.header__dropdown')) {
-    closeDropdowns()
+    closeDropdowns();
   }
-}
+};
 
 // Lifecycle hooks
 onMounted(() => {
-  checkScreenSize()
-  window.addEventListener('resize', checkScreenSize)
-  document.addEventListener('click', handleDocumentClick)
-})
+  checkScreenSize();
+  window.addEventListener('resize', checkScreenSize);
+  document.addEventListener('click', handleDocumentClick);
+});
 
 onBeforeUnmount(() => {
-  window.removeEventListener('resize', checkScreenSize)
-  document.removeEventListener('click', handleDocumentClick)
-})
+  window.removeEventListener('resize', checkScreenSize);
+  document.removeEventListener('click', handleDocumentClick);
+});
 </script>
 
 <template>
@@ -83,6 +84,7 @@ onBeforeUnmount(() => {
       <div class="header__logo">
         <img :src="logo" alt="Company Logo" class="header__logo-img">
       </div>
+      
       <button
         v-if="isMobile"
         class="header__mobile-menu-button"
@@ -91,39 +93,16 @@ onBeforeUnmount(() => {
       >
         <span class="header__mobile-menu-icon" />
       </button>
-      <nav class="header__nav" :class="{ 'header__nav--active': isMobileMenuOpen }">
-        <div
-          v-for="(config, type) in { plugin: dropdowns.plugin, theme: dropdowns.theme, pricing: dropdowns.pricing }"
-          :key="type"
-          class="header__dropdown"
-        >
-          <button
-            class="header__dropdown-button"
-            @click="toggleDropdown(type)"
-            aria-haspopup="true"
-            :aria-expanded="activeDropdown === type"
-          >
-            {{ config.selected.value }}
-            <span class="header__dropdown-arrow">
-              <img :src="dropdownArrow" alt="Dropdown arrow" class="dropdown-arrow-icon">
-            </span>
-          </button>
-
-          <ul class="header__dropdown-list" v-show="activeDropdown === type">
-            <li
-              v-for="item in config.items"
-              :key="item"
-              class="header__dropdown-item"
-              @click.stop="selectItem(type, item)"
-            >
-              {{ item }}
-            </li>
-          </ul>
-        </div>
-
-        <a href="#" class="header__nav-link">Resources</a>
-      </nav>
-
+      
+      <HeaderNav
+        :is-mobile="isMobile"
+        :dropdowns="dropdowns"
+        v-model:active-dropdown="activeDropdown"
+        @select-item="selectItem"
+        @toggle-dropdown="toggleDropdown"
+        :class="{ 'header__nav--active': isMobileMenuOpen }"
+      />
+      
       <div class="header__controls" :class="{ 'header__controls--active': isMobileMenuOpen }">
         <a href="#" class="header__nav-link">Support</a>
 
@@ -164,5 +143,5 @@ onBeforeUnmount(() => {
 </template>
 
 <style lang="scss" scoped>
-@use '../../assets/styles/components/header/Header.scss'
+@use '../../assets/styles/components/header/Header.scss';
 </style>
---
+++ my-vue-app/src/components/Header/HeaderNav.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { ref, computed } from 'vue';
+import dropdownArrow from '../../assets/icons/check_mark.svg';
+
+const props = defineProps({
+  isMobile: Boolean,
+  dropdowns: {
+    type: Object,
+    required: true
+  }
+});
+
+const emit = defineEmits(['select-item', 'toggle-dropdown']);
+
+const activeDropdown = defineModel('activeDropdown');
+
+const visibleDropdowns = computed(() => {
+  const { language, ...rest } = props.dropdowns;
+  return rest;
+});
+
+const selectItem = (type, value) => {
+  emit('select-item', { type, value });
+};
+
+const toggleDropdown = (dropdownName) => {
+  emit('toggle-dropdown', dropdownName);
+};
+</script>
+
+<template>
+  <nav class="header__nav">
+    <div
+      v-for="(config, type) in visibleDropdowns"
+      :key="type"
+      class="header__dropdown"
+    >
+      <button
+        class="header__dropdown-button"
+        @click="toggleDropdown(type)"
+        aria-haspopup="true"
+        :aria-expanded="activeDropdown === type"
+      >
+        {{ config.selected.value }}
+        <span class="header__dropdown-arrow">
+          <img :src="dropdownArrow" alt="Dropdown arrow" class="dropdown-arrow-icon">
+        </span>
+      </button>
+
+      <ul class="header__dropdown-list" v-show="activeDropdown === type">
+        <li
+          v-for="item in config.items"
+          :key="item"
+          class="header__dropdown-item"
+          @click.stop="selectItem(type, item)"
+        >
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+
+    <a href="#" class="header__nav-link">Resources</a>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+@use '../../assets/styles/components/header/Header.scss';
+</style>
