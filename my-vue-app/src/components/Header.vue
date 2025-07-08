c1834b79ed7c0c48cafcb2af9cc7a3834dea04c7
user <krasilov.kolya@yandex.ru>
2025-07-08 12:18:52 +0300

    Versia bag

--- my-vue-app/src/components/Header.vue
+++ my-vue-app/src/components/Header.vue
@@ -1,20 +1,24 @@
 <script setup>
-// 1. Vue imports
 import { ref, onMounted, onBeforeUnmount } from 'vue'
-
-// 2. Components
 import DemoButton from './DemoButton.vue'
-
-// 3. Assets
 import logo from '../assets/images/logo.png'
 import userIcon from '../assets/icons/people.svg'
+import dropdownArrow from '../assets/icons/check_mark.svg'
 
-// Component logic
 const isMobileMenuOpen = ref(false)
 const isMobile = ref(false)
+const activeDropdown = ref(null)
 
 const checkScreenSize = () => {
   isMobile.value = window.innerWidth < 768
+}
+
+const toggleDropdown = (dropdownName) => {
+  activeDropdown.value = activeDropdown.value === dropdownName ? null : dropdownName
+}
+
+const closeDropdowns = () => {
+  activeDropdown.value = null
 }
 
 onMounted(() => {
@@ -45,27 +49,81 @@ onBeforeUnmount(() => {
 
       <nav class="header__nav" :class="{ 'header__nav--active': isMobileMenuOpen }">
         <div class="header__dropdown">
-          <select class="header__dropdown-select" aria-label="Plugins dropdown">
-            <option>Plugins</option>
-            <option>Plugin 1</option>
-            <option>Plugin 2</option>
-          </select>
+          <button 
+            class="header__dropdown-button"
+            @click="toggleDropdown('plugins')"
+            @blur="closeDropdowns"
+            aria-haspopup="true"
+            :aria-expanded="activeDropdown === 'plugins'"
+          >
+            Plugins
+            <span class="header__dropdown-arrow">
+              <img 
+                :src="dropdownArrow" 
+                alt="Dropdown arrow"
+                class="dropdown-arrow-icon"
+              />
+            </span>
+          </button>
+          <ul 
+            class="header__dropdown-list"
+            v-show="activeDropdown === 'plugins'"
+          >
+            <li class="header__dropdown-item">Plugin 1</li>
+            <li class="header__dropdown-item">Plugin 2</li>
+          </ul>
         </div>
         
         <div class="header__dropdown">
-          <select class="header__dropdown-select" aria-label="Themes dropdown">
-            <option>Themes</option>
-            <option>Theme 1</option>
-            <option>Theme 2</option>
-          </select>
+          <button 
+            class="header__dropdown-button"
+            @click="toggleDropdown('themes')"
+            @blur="closeDropdowns"
+            aria-haspopup="true"
+            :aria-expanded="activeDropdown === 'themes'"
+          >
+            Themes
+            <span class="header__dropdown-arrow">
+              <img 
+                :src="dropdownArrow" 
+                alt="Dropdown arrow"
+                class="dropdown-arrow-icon"
+              />
+            </span>
+          </button>
+          <ul 
+            class="header__dropdown-list"
+            v-show="activeDropdown === 'themes'"
+          >
+            <li class="header__dropdown-item">Theme 1</li>
+            <li class="header__dropdown-item">Theme 2</li>
+          </ul>
         </div>
         
         <div class="header__dropdown">
-          <select class="header__dropdown-select" aria-label="Pricing dropdown">
-            <option>Pricing</option>
-            <option>Basic</option>
-            <option>Pro</option>
-          </select>
+          <button 
+            class="header__dropdown-button"
+            @click="toggleDropdown('pricing')"
+            @blur="closeDropdowns"
+            aria-haspopup="true"
+            :aria-expanded="activeDropdown === 'pricing'"
+          >
+            Pricing
+            <span class="header__dropdown-arrow">
+              <img 
+                :src="dropdownArrow" 
+                alt="Dropdown arrow"
+                class="dropdown-arrow-icon"
+              />
+            </span>
+          </button>
+          <ul 
+            class="header__dropdown-list"
+            v-show="activeDropdown === 'pricing'"
+          >
+            <li class="header__dropdown-item">Basic</li>
+            <li class="header__dropdown-item">Pro</li>
+          </ul>
         </div>
         
         <a href="#" class="header__nav-link">Resources</a>
@@ -73,12 +131,33 @@ onBeforeUnmount(() => {
 
       <div class="header__controls" :class="{ 'header__controls--active': isMobileMenuOpen }">
         <a href="#" class="header__nav-link">Support</a>
-        <div class="header__lang-switcher">
-          <select class="header__lang-select" aria-label="Language switcher">
-            <option value="en">En</option>
-            <option value="ru">Ru</option>
-          </select>
+        
+        <div class="header__dropdown">
+          <button 
+            class="header__dropdown-button"
+            @click="toggleDropdown('language')"
+            @blur="closeDropdowns"
+            aria-haspopup="true"
+            :aria-expanded="activeDropdown === 'language'"
+          >
+            En
+            <span class="header__dropdown-arrow">
+              <img 
+                :src="dropdownArrow" 
+                alt="Dropdown arrow"
+                class="dropdown-arrow-icon"
+              />
+            </span>
+          </button>
+          <ul 
+            class="header__dropdown-list"
+            v-show="activeDropdown === 'language'"
+          >
+            <li class="header__dropdown-item">En</li>
+            <li class="header__dropdown-item">Ru</li>
+          </ul>
         </div>
+        
         <a href="#" class="header__login-link">
           <span class="header__login-text">Login</span>
           <img :src="userIcon" alt="User icon" class="header__login-icon">
