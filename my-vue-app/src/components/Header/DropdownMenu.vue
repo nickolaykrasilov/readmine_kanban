51ecbb94b93b7d911dbba60d482de1737e28f800
user <krasilov.kolya@yandex.ru>
2025-07-10 17:29:29 +0300

    chore: Refactoring DropdownMenuu

--- my-vue-app/src/components/Header/DropdownMenu.vue
+++ my-vue-app/src/components/Header/DropdownMenu.vue
@@ -1,29 +1,3 @@
-<template>
-  <div class="header__dropdown">
-    <button
-      class="header__dropdown-button"
-      @click.stop="toggle"
-      aria-haspopup="true"
-      :aria-expanded="isOpen"
-    >
-      {{ selectedValue }}
-      <span class="header__dropdown-arrow">
-        <img :src="dropdownArrow" alt="Dropdown arrow" class="dropdown-arrow-icon">
-      </span>
-    </button>
-    <ul class="header__dropdown-list" v-show="isOpen">
-      <li
-        v-for="item in items"
-        :key="item"
-        class="header__dropdown-item"
-        @click.stop="selectItem(item)"
-      >
-        {{ item }}
-      </li>
-    </ul>
-  </div>
-</template>
-
 <script setup>
 import { ref } from 'vue';
 import dropdownArrow from '../../assets/images/icons/check-mark.svg';
@@ -51,6 +25,39 @@ const selectItem = (value) => {
   emit('select', value);
 };
 </script>
+
+<template>
+  <div class="header__dropdown">
+    <button
+      class="header__dropdown-button"
+      @click.stop="toggle"
+      aria-haspopup="true"
+      :aria-expanded="isOpen"
+    >
+      {{ selectedValue }}
+      <span class="header__dropdown-arrow">
+        <img 
+        :src="dropdownArrow" 
+        alt="Dropdown arrow" 
+        class="dropdown-arrow-icon"
+        >
+      </span>
+    </button>
+    <ul 
+    class="header__dropdown-list" 
+    v-show="isOpen"
+    >
+      <li
+        v-for="item in items"
+        :key="item"
+        class="header__dropdown-item"
+        @click.stop="selectItem(item)"
+      >
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
 
 <style lang="scss" scoped>
 @use '../../assets/styles/components/header/header.scss';
