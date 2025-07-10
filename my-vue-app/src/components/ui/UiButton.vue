41a607d422b81304de84f6624ee6259aeb9b4fcb
user <krasilov.kolya@yandex.ru>
2025-07-10 15:10:16 +0300

    fix: correct name UiButton

--- my-vue-app/src/components/ui/UiButton.vue
+++ my-vue-app/src/components/ui/UiButton.vue
@@ -1,6 +1,6 @@
 <template>
-  <button class="demo-btn">
-    <span class="demo-btn__text">
+  <button class="ui-button">
+    <span class="ui-button__text">
       Get a Free Demo
     </span>
   </button>
