9a34b2ff1634a060938e559232ef0f3a304ebb4f
user <krasilov.kolya@yandex.ru>
2025-07-11 10:09:34 +0300

    chore: Broke the link to the component

--- my-vue-app/src/components/Header/Header.vue
+++ my-vue-app/src/components/Header/Header.vue
@@ -1,43 +1,43 @@
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from 'vue';
-import { useRouter } from 'vue-router';
-import { useScreenSize } from '../../utils/screen';
-import UiButton from '../ui/UiButton.vue';
-import HeaderNav from './HeaderNav.vue';
-import LanguageSwitcher from '../LanguageSwitcher.vue';
-import logo from '../../assets/images/logo.png';
-import userIcon from '../../assets/images/icons/people.svg';
-
-const router = useRouter();
-const { isMobile } = useScreenSize();
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import { useRouter } from 'vue-router'
+import { useScreenSize } from '../../utils/screen'  
+import UiButton from '../ui/UiButton.vue'            
+import UiLink from '../ui/UiLink.vue'                
+import HeaderNav from './HeaderNav.vue'              
+import LanguageSwitcher from '../LanguageSwitcher.vue'
+import logo from '../../assets/images/logo.png'
+import userIcon from '../../assets/images/icons/people.svg'
+const router = useRouter()
+const { isMobile } = useScreenSize()
 
 // Reactive state
-const isMobileMenuOpen = ref(false);
-const activeDropdown = ref(null);
+const isMobileMenuOpen = ref(false)
+const activeDropdown = ref(null)
 
 // Dropdown configuration
 const dropdowns = {
   plugin: {
     items: ['Plugin 1', 'Plugin 2'],
-    selected: ref('Plugins'),
+    selected: ref('Plugins')
   },
   theme: {
     items: ['Theme 1', 'Theme 2'],
-    selected: ref('Themes'),
+    selected: ref('Themes')
   },
   pricing: {
     items: ['Basic', 'Pro'],
-    selected: ref('Pricing'),
+    selected: ref('Pricing')
   }
-};
+}
 
 // Methods
 const toggleDropdown = (dropdownName) => {
-  activeDropdown.value = activeDropdown.value === dropdownName ? null : dropdownName;
-};
+  activeDropdown.value = activeDropdown.value === dropdownName ? null : dropdownName
+}
 
 const selectItem = ({ type, value }) => {
-  dropdowns[type].selected.value = value;
+  dropdowns[type].selected.value = value
   const routeMap = {
     plugin: {
       'Plugin 1': '/plugins/plugin1',
@@ -45,30 +45,30 @@ const selectItem = ({ type, value }) => {
     },
     theme: {
       'Theme 1': '/themes/theme1',
-      'Theme 2': '/themes/theme2',
+      'Theme 2': '/themes/theme2'
     },
     pricing: {
       'Basic': '/pricing/basic',
-      'Pro': '/pricing/pro',
+      'Pro': '/pricing/pro'
     }
-  };
-  router.push(routeMap[type]?.[value] || '/');
-};
+  }
+  router.push(routeMap[type]?.[value] || '/')
+}
 
 const handleDocumentClick = (e) => {
   if (!e.target.closest('.header__dropdown')) {
-    activeDropdown.value = null;
+    activeDropdown.value = null
   }
-};
+}
 
 // Lifecycle hooks
 onMounted(() => {
-  document.addEventListener('click', handleDocumentClick);
-});
+  document.addEventListener('click', handleDocumentClick)
+})
 
 onBeforeUnmount(() => {
-  document.removeEventListener('click', handleDocumentClick);
-});
+  document.removeEventListener('click', handleDocumentClick)
+})
 </script>
 
 <template>
@@ -81,6 +81,7 @@ onBeforeUnmount(() => {
           class="header__logo-img"
         >
       </div>
+      
       <button
         v-if="isMobile"
         class="header__mobile-menu-button"
@@ -89,6 +90,7 @@ onBeforeUnmount(() => {
       >
         <span class="header__mobile-menu-icon" />
       </button>
+      
       <HeaderNav
         :is-mobile="isMobile"
         :dropdowns="dropdowns"
@@ -97,22 +99,31 @@ onBeforeUnmount(() => {
         @select-item="selectItem"
         :class="{ 'header__nav--active': isMobileMenuOpen }"
       />
-      <div class="header__controls" :class="{ 'header__controls--active': isMobileMenuOpen }">
-        <router-link to="/support" class="header__nav-link">
-          Support
-        </router-link>
+      
+      <div 
+        class="header__controls" 
+        :class="{ 'header__controls--active': isMobileMenuOpen }"
+      >
+        <UiLink 
+          to="/support" 
+          text="Support" 
+          class="header__nav-link"
+        />
+        
         <LanguageSwitcher />
-        <router-link to="/login" class="header__login-link">
-          <span class="header__login-text">
-            Login
-          </span>
-          <img 
-            :src="userIcon" 
-            alt="User icon" 
-            class="header__login-icon"
-          >
-        </router-link>
-        <UiButton class="header__demo-btn" />
+        
+        <UiLink 
+          to="/login" 
+          text="Login" 
+          :icon="userIcon" 
+          icon-alt="User icon"
+          is-login
+          class="header__login-link"
+        />
+        
+        <UiButton class="header__demo-btn">
+          Get Demo
+        </UiButton>
       </div>
     </div>
   </header>
---
+++ my-vue-app/src/components/ui/UiLink.vue
@@ -0,0 +1,48 @@
+<script setup>
+defineProps({
+  to: {
+    type: [String, Object],
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+    default: null
+  },
+  iconAlt: {
+    type: String,
+    default: ''
+  },
+  isLogin: {
+    type: Boolean,
+    default: false
+  }
+});
+</script>
+
+<template>
+  <router-link 
+    :to="to" 
+    :class="[
+      'ui-link',
+      { 'ui-link--login': isLogin }
+    ]"
+  >
+    <span class="ui-link__text">
+      {{ text }}
+    </span>
+    <img 
+      v-if="icon" 
+      :src="icon" 
+      :alt="iconAlt" 
+      class="ui-link__icon"
+    >
+  </router-link>
+</template>
+
+<style lang="scss" scoped>
+@use '../../assets/styles/components/ui/ui-link.scss';
+</style>
