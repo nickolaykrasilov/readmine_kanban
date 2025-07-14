f8a3553b5f27117d40644ecce0939b4cc644084a
user <krasilov.kolya@yandex.ru>
2025-07-14 11:04:01 +0300

    chore: UIButton added icon support

--- my-vue-app/src/components/ui/UIButton.vue
+++ my-vue-app/src/components/ui/UIButton.vue
@@ -7,6 +7,14 @@ defineProps({
   disabled: {
     type: Boolean,
     default: false
+  },
+  iconLeft: {
+    type: String,
+    default: null
+  },
+  iconRight: {
+    type: String,
+    default: null
   }
 });
 </script>
@@ -17,11 +25,20 @@ defineProps({
     :class="{ 'is-disabled': disabled }"
     :disabled="disabled"
   >
+    <span v-if="iconLeft" class="ui-button__icon ui-button__icon--left">
+      <img :src="iconLeft" alt="" aria-hidden="true" />
+    </span>
+    
     <span class="ui-button__text">
       {{ label }}
     </span>
+    
+    <span v-if="iconRight" class="ui-button__icon ui-button__icon--right">
+      <img :src="iconRight" alt="" aria-hidden="true" />
+    </span>
   </button>
 </template>
+
 
 <style lang="scss" scoped>
 @use '../../assets/styles/components/ui/ui-button.scss';
