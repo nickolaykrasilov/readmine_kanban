f373178d0f5de50d5f4a62b3adde1e6f53a76e8b
user <krasilov.kolya@yandex.ru>
2025-07-15 12:12:11 +0300

    feat: Caution! svg icons are inserted here, not as a path.

--- my-vue-app/src/components/LanguageSwitcher.vue
+++ my-vue-app/src/components/LanguageSwitcher.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
-import dropdownArrow from '../assets/images/icons/check-mark.svg';
 import { LANGUAGES, DEFAULT_LANGUAGE } from '../models/languages.js';
 
 const currentLanguage = ref(DEFAULT_LANGUAGE);
@@ -47,10 +46,18 @@ const switchLanguage = (langCode) => {
     >
       {{ currentLanguage }}
       <span class="language-switcher__arrow">
-        <img
-          :src="dropdownArrow"
-          alt="Dropdown arrow"
+        <svg 
+        width="10" 
+        height="6" 
+        viewBox="0 0 10 6" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
         >
+          <path 
+          d="M0.638672 1L5.13867 5L9.63867 1" 
+          stroke="#3D3D3D"
+          />
+        </svg>
       </span>
     </button>
     <ul
