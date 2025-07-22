006bacb47b52c22e33c94322735dd44b13d2e199
user <krasilov.kolya@yandex.ru>
2025-07-22 14:32:01 +0300

    chore: extra div removed

--- my-vue-app/src/components/MainBanner/MainBanner.vue
+++ my-vue-app/src/components/MainBanner/MainBanner.vue
@@ -16,14 +16,13 @@ import { cards } from '../../models/CardsData.js';
             Powerful Agile & Kanban solutions for Redmine
         </div>
         <div class="main-banner__text-block">
-          <div class="subtitle">
             <p class="main-banner__subtitle">
+              <div class="subtitle">
               Our plugins will help you stay organized and 
               ensure that all necessary tasks are completed 
               throughout the project lifecycle.
+              </div>
             </p>
-          </div>
-          <div class="main-banner__button-wrapper">
             <UIButton class="main-banner__button">
               Get a Free Demo
               <template #icon>
@@ -32,7 +31,6 @@ import { cards } from '../../models/CardsData.js';
                 </span>
               </template>
             </UIButton>
-          </div>
         </div>
         <img
           src="../../assets/images/screen.jpg"
