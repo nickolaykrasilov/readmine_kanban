1ca9e7d105a2e1995223c8e379f77089579dc67f
user <krasilov.kolya@yandex.ru>
2025-08-22 16:01:16 +0300

    chore: an extra class has been removed

--- my-vue-app/src/components/Footer/Footer.vue
+++ my-vue-app/src/components/Footer/Footer.vue
@@ -1,46 +1,45 @@
 <script setup>
 import Logo from '../Logo.vue';
-import FooterForm from './FooterForm.vue'; 
+import FooterForm from './FooterForm.vue';
 import UILink from '../ui/UILink.vue';
 </script>
 
 <template>
   <footer class="footer">
-    <div class="footer__container">
-      <div class="footer__content">
-        <div class="footer__info">
-          <Logo size="60" />
-          <div class="footer__links">
-            <UILink
-              href="#"
-              label="Privacy Policy"
-              theme="white"
-            />
-            <UILink
-              href="#"
-              label="Contacts"
-              theme="white"
-            />
-            <UILink
-              href="#"
-              label="Terms and Conditions"
-              theme="white"
-            />
-            <UILink
-              :href="`mailto:${email}`"
-              :label="'support@redmine-kanban.com'"
-              theme="white"
-            />
-          </div>
+    <div class="footer__content">
+      <div class="footer__info">
+        <Logo size="60" />
+        <div class="footer__links">
+          <UILink
+            href="#"
+            label="Privacy Policy"
+            theme="white"
+          />
+          <UILink
+            href="#"
+            label="Contacts"
+            theme="white"
+          />
+          <UILink
+            href="#"
+            label="Terms and Conditions"
+            theme="white"
+          />
+          <UILink
+            :href="`mailto:${email}`"
+            :label="'support@redmine-kanban.com'"
+            theme="white"
+          />
         </div>
-        
-        <FooterForm
-          :title="`Our plugins will help you stay organized and
+      </div>
+      
+      <FooterForm
+        :title=
+          "`Our plugins will help you stay organized and
           ensure that all necessary tasks are completed
           throughout the project lifecycle.`"
-          :description="'Fill out the form and we will contact you'"
-        />
-      </div>
+        :description="'Fill out the form and we will contact you'"
+      />
     </div>
   </footer>
 </template>
