2d02a418c028e8115c4e05bce3f314cb77f6d989
user <krasilov.kolya@yandex.ru>
2025-08-22 09:31:03 +0300

    fix: changed the props to ref in footer

--- my-vue-app/src/components/Footer.vue
+++ my-vue-app/src/components/Footer.vue
@@ -1,43 +1,39 @@
 <script setup>
+import { ref } from 'vue';
+
 import UIButton from './ui/UIButton.vue';
 import UILink from './ui/UILink.vue';
 
-const props = defineProps({
-  title: {
-    type: String,
-    default: '',
-  },
-  description: {
-    type: String,
-    default: '',
+const formData = ref({
+  name: '',
+  email: '',
+  type: '',
+  text: '',
+})
+
+const formFields = ref({
+  name: {
+    placeholder: 'Name',
   },
   email: {
-    type: String,
-    default: '',
+    placeholder: 'Email',
   },
-  formFields: {
-    type: Object,
-    default: () => ({
-      name: {
-        placeholder: 'Name',
-      },
-      email: {
-        placeholder: 'Email',
-      },
-      type: {
-        options: [
-          'Option 1',
-          'Option 2',
-          'Option 3',
-        ],
-        placeholder: 'Type',
-      },
-      text: {
-        placeholder: 'Text',
-      },
-    })
+  type: {
+    options: [
+      'Option 1',
+      'Option 2',
+      'Option 3',
+    ],
+    placeholder: 'Type',
+  },
+  text: {
+    placeholder: 'Text',
   },
 });
+
+const email = ref('support@redmine-kanban.com')
+const title = ref('Our plugins will help you stay organized')
+const description = ref('Fill out the form and we will contact you')
 </script>
 
 <template>
@@ -72,64 +68,74 @@ const props = defineProps({
             />
             <UILink
               :href="`mailto:${email}`"
-              label="support@redmine-kanban.com"
+              :label="email"
               theme="white"
             />
           </div>
         </div>
         <div class="footer__form">
           <h2 class="footer__form-title">
-            {{
-            `Our plugins will help you stay organized and
-            ensure that all necessary tasks are completed
-            throughout the project lifecycle.`
-            }}
+            {{ title }}
           </h2>
           <h3 class="footer__form-description description">
-            {{ 'Fill out the form and we will contact you' }}
+            {{ description }}
           </h3>
-          <form class="footer__form-container ">
+          <form 
+            class="footer__form-container"
+            @submit.prevent="handleSubmit"
+          >
             <div class="footer__form-container__group">
               <input
+                v-model="formData.name"
                 type="text"
                 class="footer__form-input"
                 :placeholder="formFields.name.placeholder"
+                required
               >
             </div>
             <div class="footer__form-container__group">
               <input
+                v-model="formData.email"
                 type="email"
                 class="footer__form-input"
                 :placeholder="formFields.email.placeholder"
+                required
               >
             </div>
             <div class="footer__form-container-group">
-                <select class="footer__form-select footer__form-input">
-                  <option value="" disabled selected>
-                    {{ formFields.type.placeholder }}
-                  </option>
-                  <option
-                    v-for="(option, index) in formFields.type.options"
-                    :key="index"
-                    :value="option"
-                  >
-                    {{ option }}
-                  </option>
-                </select>
-                <triangleIcon class="footer__form-select-icon" />
+              <select 
+                v-model="formData.type"
+                class="footer__form-select footer__form-input"
+                required
+              >
+                <option value="" disabled>
+                  {{ formFields.type.placeholder }}
+                </option>
+                <option
+                  v-for="(option, index) in formFields.type.options"
+                  :key="index"
+                  :value="option"
+                >
+                  {{ option }}
+                </option>
+              </select>
+              <triangleIcon class="footer__form-select-icon" />
             </div>
             <div class="footer__form-group">
               <textarea
+                v-model="formData.text"
                 class="footer__form-textarea"
                 :placeholder="formFields.text.placeholder"
-              />
+                required
+              ></textarea>
             </div>
             <UIButton
+              type="submit"
               label="Send"
               class="footer__form-button"
               theme="blue"
             >
-              <template #icon >
+              <template #icon>
                 <whiteArrowIcon />
               </template>
             </UIButton>
